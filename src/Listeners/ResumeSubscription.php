b6c48a56e03a0a4ef82c0a5893aa9e3a0e622831
Faruk Nasir <frknasir@yahoo.com>
2021-03-05 23:26:58 +0100

    fix: bug in resumesubscription listener

--- src/Listeners/ResumeSubscription.php
+++ src/Listeners/ResumeSubscription.php
@@ -8,13 +8,10 @@ class ResumeSubscription
 {
     public function handle(InvoicePaid $event)
     {
-        $invoice = $event->invoice;
         $subscription = $event->invoice->subscription;
 
         if ($subscription->cancelled()) {
             $subscription->resume();
-            $invoice->due_date = now();
-            $invoice->save();
         }
     }
 }
