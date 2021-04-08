95ad1b5f1868fdd8168f81e9c39deca60a6d223e
Faruk Nasir <frknasir@yahoo.com>
2021-04-08 16:48:54 +0100

    chore: written some tests

---
+++ tests/Feature/PlanTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace StarfolkSoftware\Paysub\Tests\Feature;
+
+use StarfolkSoftware\Paysub\Models\Plan;
+
+class PlanTest extends FeatureTestCase {
+    /**
+     * @var Plan
+     */
+    protected static $basicPlan;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::$basicPlan = Plan::create([
+            'name' => 'basic',
+            'display_name' => 'Basic',
+            'description' => '',
+            'interval_type' => 'monthly',
+            'interval_count' => 1,
+            'amount' => 250000,
+        ]);
+    }
+
+    public function test_plan_has_feature() {
+        self::$basicPlan->features()->create([
+            'name' => 'sample_feature',
+            'sort_order' => 1
+        ]);
+
+        $this->assertTrue(self::$basicPlan->hasFeature('sample_feature'));
+    }
+}
